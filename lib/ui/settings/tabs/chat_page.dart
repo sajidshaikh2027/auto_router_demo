3d1d448a2457fb7736e9b318231f34c065f1cee4
sajid shaikh <sajid.shaikh@brainvire.com>
2024-08-07 17:43:17 +0530

    Changes in code implement page view

--- lib/ui/settings/tabs/chat_page.dart
+++ lib/ui/settings/tabs/chat_page.dart
@@ -1,45 +1,50 @@
-import 'package:auto_router_demo/ui/settings/tabs/widget/simple_card.dart';
-import '../../../utils/exports.dart';
+  import 'package:auto_router_demo/ui/settings/tabs/widget/simple_card.dart';
+  import 'package:flutter/gestures.dart';
+  import '../../../utils/exports.dart';
 
-@RoutePage()
-class ChatPage extends StatelessWidget {
-  const ChatPage({super.key});
+  @RoutePage()
+  class ChatPage extends StatelessWidget {
+    const ChatPage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    final List<CardData> cardDataList = [
-      CardData(
-          title: "Card One",
-          description: "This is the description for card one."),
-      CardData(
-          title: "Card Two",
-          description: "This is the description for card two."),
-      CardData(
-          title: "Card Three",
-          description: "This is the description for card three."),
-    ];
+    @override
+    Widget build(BuildContext context) {
+      final List<CardData> cardDataList = [
+        CardData(
+            title: "Card One",
+            description: "This is the description for card one."),
+        CardData(
+            title: "Card Two",
+            description: "This is the description for card two."),
+        CardData(
+            title: "Card Three",
+            description: "This is the description for card three."),
+      ];
 
-    return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(
-            height: 200,
-            child: AutoTabsRouter.pageView(
-              // physics: NeverScrollableScrollPhysics(),
-              routes: cardDataList
-                  .map(
-                    (cardData) => RouteViewOne(
-                        title: cardData.title,
-                        description: cardData.description),
-                  )
-                  .toList(), // Convert to List
+      return Scaffold(
+        body: Column(
+          children: [
+            SizedBox(
+              height: 150,
+              child: AutoTabsRouter.pageView(
+                routes: cardDataList
+                    .map(
+                      (cardData) => RouteViewOne(
+                          title: cardData.title,
+                          description: cardData.description),
+                    )
+                    .toList(),
+                builder: (context,child, controller){
+                  return child;
+                },
+
+                // Convert to List
+              ),
             ),
-          ),
-          Text("data"),
-          Text("data"),
-          Text("data"),
-        ],
-      ),
-    );
+            Text("data"),
+            Text("data"),
+            Text("data"),
+          ],
+        ),
+      );
+    }
   }
-}
